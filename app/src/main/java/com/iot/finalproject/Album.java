e7ce19c035723cc75f51f2170e990ebea6fda9f4
chingline <chingline@naver.com>
2017-07-06 16:56:07 +0900

    사용자모드 recycleerview

---
+++ app/src/main/java/com/iot/finalproject/Album.java
@@ -0,0 +1,35 @@
+package com.iot.finalproject;
+
+/**
+ * Created by CHJ on 2017-07-06.
+ */
+
+public class Album {
+    private String title;
+    private String artist;
+    private int image;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
--- app/src/main/java/com/iot/finalproject/MainActivity.java
+++ app/src/main/java/com/iot/finalproject/MainActivity.java
@@ -1,7 +1,56 @@
 package com.iot.finalproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
+    private RecyclerView lecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initLayout();
+        initData();
+    }
+
+    /**
+     * 레이아웃 초기화
+     */
+    private void initLayout(){
+
+        lecyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+    }
+
+    /**
+     * 데이터 초기화
+     */
+    private void initData(){
+
+        List<Album> albumList = new ArrayList<Album>();
+
+        for (int i =0; i<20; i ++){
+
+            Album album = new Album();
+            album.setTitle("어느 멋진 날");
+            album.setArtist("정용");
+            album.setImage(R.drawable.a);
+            albumList.add(album);
+        }
+
+        lecyclerView.setAdapter(new MyRecyclerAdapter(albumList,R.layout.row_album));
+        lecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        lecyclerView.setItemAnimator(new DefaultItemAnimator());
+
+    }
+
 }
+
 
